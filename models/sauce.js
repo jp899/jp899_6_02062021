4cb262f2b4d8f56160161ad7b6f6531db11e7f30
Jean-Paul Navailles <navailles.jp3@gmail.com>
2021-06-02 19:55:43 +0200

    Added route to like/dislike/unlike a sauce

--- models/sauce.js
+++ models/sauce.js
@@ -12,9 +12,41 @@ const sauceSchema = mongoose.Schema({
   imageUrl: { type: String, required: true },
   heat: { type: Number, required: true },
   likes: { type: Number, default: 0 },
-  dislikes: { type: [String], default: 0 },
+  dislikes: { type: Number, default: 0 },
   usersLiked: { type: [String], default: [] },
   usersDisliked: { type: [String], default: [] },
 });
+
+
+// Définition de méthodes sur les instances de sauce
+
+sauceSchema.methods.isLiked = function(userId) {
+  return this.usersLiked.includes(userId);
+}
+
+sauceSchema.methods.isDisliked = function(userId) {
+  return this.usersDisliked.includes(userId);
+}
+
+sauceSchema.methods.like = function(userId) {
+  this.usersLiked.push(userId);
+  this.likes ++;
+}
+
+sauceSchema.methods.dislike = function(userId) {
+  this.usersDisliked.push(userId);
+  this.dislikes ++;
+}
+
+sauceSchema.methods.unLike = function(userId) {
+  this.usersLiked = this.usersLiked.filter(function(user){ return user !== userId;});
+  this.likes --;
+}
+
+sauceSchema.methods.unDislike = function(userId) {
+  this.usersDisliked = this.usersDisliked.filter(function(user){ return user !== userId;});
+  this.dislikes --;
+}
+
 
 module.exports = mongoose.model('Sauce', sauceSchema);
