a3fc983f818a82043c7115c992032091dfc7f7d4
Jean-Paul Navailles <navailles.jp3@gmail.com>
2021-06-02 16:32:07 +0200

    Modified PUT Sauce : Delete old image from the files when a new image is sent by the client

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -32,6 +32,22 @@ exports.getOneSauce = (req, res, next) => {
 };
 
 exports.modifySauce = (req, res, next) => {
+
+    // Suppression de l'ancienne image si le fichier a été modifié par l'user
+    if(req.file){
+      Sauce.findOne({ _id: req.params.id })
+      .then(sauce => {
+        // Récuperer l'adresse du fichier lié à l'objet
+        const filename = sauce.imageUrl.split('/images/')[1];
+        // Supprimer ce fichier
+        fs.unlink(`images/${filename}`,(err => {
+          if (err) console.log(err);
+          else console.log(`Fichier supprimé : ${filename}`);
+        }));
+      })
+      .catch(error => res.status(500).json({ error }));
+    }
+
     const sauceObject = req.file ?
     // Si un fichier a été inclus dans la requette (fichier modifié par lutilisateur)
     // Alors on traite l'image
