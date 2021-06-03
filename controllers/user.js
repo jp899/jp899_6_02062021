c110432374b55f9beb9437343c453cf94b7f471e
Jean-Paul Navailles <navailles.jp3@gmail.com>
2021-06-03 09:32:51 +0200

    Added password and email validation before signup

--- controllers/user.js
+++ controllers/user.js
@@ -2,14 +2,34 @@ const User = require('../models/user.js');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
+// Paramétrage dela dificulté de hashage
+const saltRounds = 10;
+// paramétrage du token de session
 const sessionTokenSecret = 'RANDOM_TOKEN_SECRET';
 const sessionTokenExpiresDelay = '24h';
 
+// Regex de contrôle des entrées utilisateur :
 
-// fonction de création d'un nouvel user
+// EMAIL : Same regex used to check type="email" input in HTML5.
+// Remark : it allows emails without Top Level Domain (ex : admin@mailserver1), which are rare, but possible and valid emails.
+const emailRegex = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+// PASSWORD : de 8 à 15 caractères avec au moins : 1 minuscule, 1 majuscule, un chiffre, un caractère spécial 
+const passwordRegex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[-+!*$@%#=_])([-+!*$@%#=_\w]{8,15})$/ ;
+
+
+// Fonction de création d'un nouvel user
 exports.signup = (req, res, next) => {
-    // paramètre 10 = difficulté du hashage
-    bcrypt.hash(req.body.password, 10)
+
+  // Validation des entrées utilisateur
+  if(! emailRegex.test(req.body.email)){
+    return res.status(400).json({ error: "Format de l'email incorrect !" });
+  }
+  else if(! passwordRegex.test(req.body.password)){
+    return res.status(400).json({ error: 'Format du mot de passe incorrect !' });
+  }
+  else{
+    // Auto-génération d'un salt et hashage
+    bcrypt.hash(req.body.password, saltRounds)
       .then(hash => {
         const user = new User({
           email: req.body.email,
@@ -20,9 +40,10 @@ exports.signup = (req, res, next) => {
           .catch(error => res.status(400).json({ error }));
       })
       .catch(error => res.status(500).json({ error }));
+    } 
   };
 
-// fonction d'authentification
+// Fonction d'authentification
   exports.login = (req, res, next) => {
     User.findOne({ email: req.body.email })
       .then(user => {
